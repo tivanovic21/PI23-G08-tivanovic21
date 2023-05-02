c8269e05a12ee2f80fc53a6849f10b9df64c5fdf
Programsko Inzenjerstvo <pi_foi@outlook.com>
2023-05-02 13:28:52 +0200

    Vjezba 2

---
+++ Evaluation Manager/Models/Teacher.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evaluation_Manager.Models
+{
+    public class Teacher : Person
+    {
+        public string Username
+        {
+            get; set;
+        }
+
+        public string Password
+        {
+            get; set;
+        }
+    } // public class Teacher
+}
